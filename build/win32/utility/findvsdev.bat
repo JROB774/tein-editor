15c0a5506a21e0e6bef1da84c7d53aa7b538f4ba
Joshua Robertson <jrobertson774@yahoo.com>
2020-05-10 01:41:25 +0100

    Added support for MS Build Tools in the build files.

--- build/win32/utility/findvsdev.bat
+++ build/win32/utility/findvsdev.bat
@@ -8,7 +8,7 @@ REM ============================================================================
 REM ============================================================================
 
 set VSWhere="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
-for /f "usebackq tokens=1* delims=: " %%i in (`%VSWhere% -latest -requires Microsoft.Component.MSBuild`) do (
+for /f "usebackq tokens=1* delims=: " %%i in (`%VSWhere% -latest -requires Microsoft.Component.MSBuild -products *`) do (
     if /i "%%i"=="installationPath" set VSDevPath=%%j
 )
 set VSDevPath="%VSDevPath%\Common7\Tools\vsdevcmd.bat"
