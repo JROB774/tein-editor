0d8138282efe0bd479c0e9b8029202df332a9aa0
Joshua Robertson <joshrobertson774@gmail.com>
2021-02-17 18:10:06 +0000

    Updated the version number to v1.0.14.

--- source/application.hpp
+++ source/application.hpp
@@ -2,11 +2,7 @@
 
 GLOBAL constexpr int APP_VER_MAJOR = 1;
 GLOBAL constexpr int APP_VER_MINOR = 0;
-GLOBAL constexpr int APP_VER_PATCH = 13;
-GLOBAL constexpr int APP_VER_BUILD = 0;
-
-GLOBAL constexpr const char* APP_VER_STR = "1.0.13.0";
-GLOBAL constexpr int         APP_VER_NUM =  11300;
+GLOBAL constexpr int APP_VER_PATCH = 14;
 
 GLOBAL SDL_Event main_event;
 GLOBAL bool      main_running;
