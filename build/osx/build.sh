2cf896788365ad340ca7f6c6d38beae7e3924224
Joshua Robertson <joshrobertson774@gmail.com>
2021-02-17 20:57:35 +0000

    Added build script for OSX.

---
+++ build/osx/build.sh
@@ -0,0 +1,14 @@
+
+IncludeDirs="-I ../../third_party/freetype/include -I ../../third_party/freetype/include/freetype -I ../../third_party/glad -I ../../third_party/glm -I ../../third_party/gon -I ../../third_party/sdl2/include -I ../../third_party/stb"
+LibraryDirs="-L ../../third_party/freetype/lib/osx"
+
+Libraries="-lfreetype"
+
+Frameworks="-F ../../third_party/sdl2/lib/osx -framework SDL2 -rpath ../../third_party/sdl2/lib/osx"
+
+Defines="-D PLATFORM_OSX"
+
+cd ../../binary/osx
+g++ -std=c++17 ../../source/main.cpp -o TheEndEditor $IncludeDirs $Defines $LibraryDirs $Libraries $Frameworks
+
+#  -I ./SDL2/SDL2.framework/Headers -F ./SDL2 -framework SDL2 -rpath ./SDL2
