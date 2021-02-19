5d3d786eb20190c54f2f4e2d35027ecdb6d25642
Joshua Robertson <joshrobertson774@gmail.com>
2021-02-19 23:51:08 +0000

    Temporary fixed executable path on OSX for testing.

--- source/platform/osx/platform.cpp
+++ source/platform/osx/platform.cpp
@@ -45,7 +45,7 @@ STDDEF void setup_crash_handler ()
 STDDEF std::string get_executable_path ()
 {
     // @Incomplete: ...
-    return "";
+    return "/Users/admin/Workspace/tein-editor/binary/osx/";
 }
 FILDEF bool run_executable (std::string exe)
 {
