dad60edcedfdfd0b141a36db1c584171244d2019
Joshua Robertson <joshrobertson774@gmail.com>
2021-04-21 16:47:05 +0100

    Update version number to v1.0.16.

--- source/application.hpp
+++ source/application.hpp
@@ -2,7 +2,7 @@
 
 GLOBAL constexpr int APP_VER_MAJOR = 1;
 GLOBAL constexpr int APP_VER_MINOR = 0;
-GLOBAL constexpr int APP_VER_PATCH = 15;
+GLOBAL constexpr int APP_VER_PATCH = 16;
 
 GLOBAL constexpr const char* CRASH_DUMP_PATH = "crashes/";
 GLOBAL constexpr const char* BACKUPS_PATH = "backups/";
